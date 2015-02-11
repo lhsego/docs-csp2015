b427cc2722e46c850be19df883a950534726b970
Sego, Landon H <landon.sego@pnnl.gov>
2015-02-10 18:04:28 -0800

    Updating by calling buildDocs()

--- code/download_instructions.R
+++ code/download_instructions.R
@@ -1,8 +1,9 @@
 
 
-   install.packages(c("devtools", "rJava", "plyr"))
+   install.packages(c("devtools", "rJava", "plyr", "maps"))
    library(devtools)
    install_github("tesseradata/datadr")
    install_github("tesseradata/trelliscope")
+   install_github("hafen/housingData")
 
 
